119476bd86aee30ecacfb28d75e11e4732f62d6d
Patrick Armentrout <patrick.armentrout@gmail.com>
2020-04-12 11:40:04 -0600

    Start playing with font awesome

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 $(() => {
     hideErrors();
-    let hasFinalSolved = false;
     // If you are reading this, it is cheating.  I'm not mad, just disappointed...
     const doorData = [{
       number: 1,
-      password: 'test'
+      password: 'test1',
+      solved: false
     },
     {
       number: 2,
-      password: 'test'
+      password: 'test2',
+      solved: false
     },
     {
       number: 3,
-      password: 'test'
+      password: 'test3',
+      solved: false
     }];
 
     function toggleLeft(element) {
@@ -25,35 +27,36 @@ $(() => {
 
     for (let door of doorData) {
       $(`#door${door.number}-form`).click((event) => {
-        openDoor(door.number, door.password);
+        openDoor(door);
       });
     }
 
-    $('#final-form').click((event) => {
-      let password = $('#final-code').first().val().toLowerCase();
-      console.log(password);
-      if (password == "270985") { // doors 5, 3, 6, 4, 2, 1
-        toggleDoor(document.querySelector('#door7'));
-        if (!hasFinalSolved) $('#myModal').modal();
-        hasFinalSolved = true;
-        hideError(7);
+    function openDoor(door) {
+      let password = $(`#door${door.number}-code`).first().val().toLowerCase();
+
+      if (password === door.password) {
+        door.solved = true;
+        const finalSolve = checkAll();
+        if (finalSolve) {
+          $('#myModal').modal();
+        }
+        console.log(password + ' ' + door.password);
+        toggleLeft(document.querySelector(`#curtain${door.number}-left`))
+        toggleRight(document.querySelector(`#curtain${door.number}-right`))        
+        hideError(door.number);
       } else {
-        showError(7);
+        door.solved = false;
+        showError(door.number);
       }
+    }
 
-    });
-
-    function openDoor(doorNumber, doorPassword) {
-      let password = $(`#door${doorNumber}-code`).first().val().toLowerCase();
-
-      if (password === doorPassword) {
-        console.log(password + ' ' + doorPassword);
-        toggleLeft(document.querySelector(`#curtain${doorNumber}-left`))
-        toggleRight(document.querySelector(`#curtain${doorNumber}-right`))        
-        hideError(doorNumber);
-      } else {
-        showError(doorNumber);
+    function checkAll() {
+      for (let door of doorData) {
+        if (!door.solved) {
+          return false;
+        }
       }
+      return true;
     }
 
     function showError(door) {
@@ -68,9 +71,5 @@ $(() => {
       $('#door1Error').hide();
       $('#door2Error').hide();
       $('#door3Error').hide();
-      $('#door4Error').hide();
-      $('#door5Error').hide();
-      $('#door6Error').hide();
-      $('#door7Error').hide();
     }
 })
